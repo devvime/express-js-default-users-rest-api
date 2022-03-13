13474d0ab40390c5f0f26baff8c053cca028657b
Victor Alves <victor.alves@leadforce.com.br>
2022-03-13 12:18:37 -0300

    add cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var bodyParser = require('body-parser')
 var express = require("express")
 var app = express()
 var router = require("./routes/routes")
- 
+var cors = require('cors')
+
+app.use(cors()) 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
